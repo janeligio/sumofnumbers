dcff65b0e98657c8cc0b99cc245d68cebf550566
janeligio <jeligio808@gmail.com>
2017-09-17 21:42:47 -1000

    wod1

---
+++ sumofnumbers.js
@@ -0,0 +1,33 @@
+/*
+Write four functions that return the sum of the numbers in a given list using a for-loop, a while-loop, recursion, and underscore. Call them sumFor, sumWhile, sumRecursion, and sumTheSimpleWay.
+
+ */
+function sumFor(list){
+    let total = 0;
+    for(let i = 0; i < list.length; i++){
+      total += list[i];
+    }
+    return total;
+}
+
+function sumWhile(list) {
+    let total = 0;
+    let i = 0;
+    while(i < list.length){
+      total += list[i];
+      i++;
+    }
+    return total;
+}
+
+function sumRecursion(list, num){
+    if(num === 0){
+      return list[num];
+    } else {
+      return list[num] + sumRecursion(list, num - 1);
+    }
+}
+
+function sumTheSimpleWay(list) {
+    return _.reduce(list, function(memo, num){ return memo + num; }, 0);
+}
